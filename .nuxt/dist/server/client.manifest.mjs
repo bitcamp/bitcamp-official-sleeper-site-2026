841cb3f182c426f6d4bd78c2889454f7ddd10530
Saloni Shah <2023sshah@tjhsst.edu>
2025-06-30 19:06:23 -0400

    2026 sleeper site base

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,18 @@
+export default {
+  "@vite/client": {
+    "prefetch": true,
+    "isEntry": true,
+    "file": "@vite/client",
+    "css": [],
+    "module": true,
+    "resourceType": "script"
+  },
+  "/Users/saloni/Desktop/Everything/School/College/Clubs/Bitcamp/bitcamp-official-sleeper-site-2026/node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "isEntry": true,
+    "file": "/Users/saloni/Desktop/Everything/School/College/Clubs/Bitcamp/bitcamp-official-sleeper-site-2026/node_modules/nuxt/dist/app/entry.js"
+  }
+}
